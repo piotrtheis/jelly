03a6aced0cb07660ba1c454ac2f874f98e512d69
tyson <piotr_theis@o2.pl>
2016-02-20 14:47:20 +0100

    add unique to slug

--- classes/jelly/core/field/slug.php
+++ classes/jelly/core/field/slug.php
@@ -1,4 +1,7 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php
+
+defined('SYSPATH') or die('No direct script access.');
+
 /**
  * Handles "slugs"
  *
@@ -13,84 +16,104 @@
  * @copyright  (c) 2010-2011 Jonathan Geiger
  * @license    http://www.opensource.org/licenses/isc-license.txt
  */
-abstract class Jelly_Core_Field_Slug extends Jelly_Field_String {
+abstract class Jelly_Core_Field_Slug extends Jelly_Field_String
+{
 
-	/**
-	 * @var  boolean  transliterate to ASCII
-	 */
-	public $ascii_only = TRUE;
+    /**
+     * @var  boolean  transliterate to ASCII
+     */
+    public $ascii_only = TRUE;
 
-	/**
-	 * @var  string  separator in slug
-	 */
-	public $separator = '-';
+    /**
+     * @var  string  separator in slug
+     */
+    public $separator = '-';
 
-	/**
-	 * @var  string  hierarchy separator in slug, if set you
-	 *               can have slugs like my-category/my-subcategory,
-	 *               where the hierarchy separator is set to '/'
-	 */
-	public $hierarchy_separator;
+    /**
+     * @var  string  hierarchy separator in slug, if set you
+     *               can have slugs like my-category/my-subcategory,
+     *               where the hierarchy separator is set to '/'
+     */
+    public $hierarchy_separator;
 
-	/**
-	 * Converts a slug to value valid for a URL.
-	 *
-	 * @param    mixed  $value
-	 * @return   mixed
-	 * @uses     UTF8::transliterate_to_ascii
-	 * @credits  Kohana-Team
-	 */
-	public function set($value)
-	{
-		list($value, $return) = $this->_default($value);
+    /**
+     * Converts a slug to value valid for a URL.
+     *
+     * @param    mixed  $value
+     * @return   mixed
+     * @uses     UTF8::transliterate_to_ascii
+     * @credits  Kohana-Team
+     */
+    public function set($value)
+    {
+        list($value, $return) = $this->_default($value);
 
-		if ( ! $return)
-		{
-			if ($this->ascii_only === TRUE)
-			{
-				// Transliterate value to ASCII
-				$value = UTF8::transliterate_to_ascii($value);
-			}
+        if (!$return)
+        {
+            if ($this->ascii_only === TRUE)
+            {
+                // Transliterate value to ASCII
+                $value = UTF8::transliterate_to_ascii($value);
+            }
 
-			// Set preserved characters
-			$preserved_characters = preg_quote($this->separator);
+            // Set preserved characters
+            $preserved_characters = preg_quote($this->separator);
 
-			// Add hierarchy separator to preserved characters if set
-			if ($this->hierarchy_separator)
-			{
-				$preserved_characters .= preg_quote($this->hierarchy_separator);
-			}
+            // Add hierarchy separator to preserved characters if set
+            if ($this->hierarchy_separator)
+            {
+                $preserved_characters .= preg_quote($this->hierarchy_separator);
+            }
 
-			// Remove all characters that are not in preserved characters, a-z, 0-9, or whitespace
-			$value = preg_replace('![^'.$preserved_characters.'a-z0-9\s]+!', '', strtolower($value));
+            // Remove all characters that are not in preserved characters, a-z, 0-9, or whitespace
+            $value = preg_replace('![^' . $preserved_characters . 'a-z0-9\s]+!', '', strtolower($value));
 
-			// Remove whitespace around hierarchy separators if hierarchy separator is set
-			if ($this->hierarchy_separator)
-			{
-				$value = preg_replace('/\s*(['.preg_quote($this->hierarchy_separator, '/').'])\s*/', '$1', $value);
-			}
+            // Remove whitespace around hierarchy separators if hierarchy separator is set
+            if ($this->hierarchy_separator)
+            {
+                $value = preg_replace('/\s*([' . preg_quote($this->hierarchy_separator, '/') . '])\s*/', '$1', $value);
+            }
 
-			// Replace all separator characters and whitespace by a single separator
-			$value = preg_replace('!['.preg_quote($this->separator).'\s]+!u', $this->separator, $value);
+            // Replace all separator characters and whitespace by a single separator
+            $value = preg_replace('![' . preg_quote($this->separator) . '\s]+!u', $this->separator, $value);
 
-			// Trim separators from the beginning and end
-			$value = trim($value, $this->separator);
+            // Trim separators from the beginning and end
+            $value = trim($value, $this->separator);
 
-			// Check if hierarchy separator is set
-			if ($this->hierarchy_separator)
-			{
-				// Replace all hierarchy separators by a single hierarchy separator
-				$value = preg_replace('!['.preg_quote($this->hierarchy_separator).']+!u', $this->hierarchy_separator, $value);
+            // Check if hierarchy separator is set
+            if ($this->hierarchy_separator)
+            {
+                // Replace all hierarchy separators by a single hierarchy separator
+                $value = preg_replace('![' . preg_quote($this->hierarchy_separator) . ']+!u', $this->hierarchy_separator, $value);
 
-				// Trim hierarchy separators from the beginning and end
-				$value = trim($value, $this->hierarchy_separator);
+                // Trim hierarchy separators from the beginning and end
+                $value = trim($value, $this->hierarchy_separator);
 
-				// Look for separators again at the beginning and end just in case
-				$value = trim($value, $this->separator);
-			}
-		}
+                // Look for separators again at the beginning and end just in case
+                $value = trim($value, $this->separator);
+            }
+        }
 
-		return $value;
-	}
+        return $this->_unique($value);
+    }
 
-} // End Jelly_Core_Field_Slug
+    /**
+     * Find unique slug
+     * 
+     * @param string $slug converted slug
+     * @return string unique slug
+     */
+    protected final function _unique($slug)
+    {
+        //find all occurrences
+        $items = Jelly::query($this->model)->where($this->name, 'LIKE', $slug . '%')->count();
+        
+        if((bool)$items)
+            return ($slug . '-' . ($items + 1));
+        
+        return $slug;
+    }
+
+}
+
+// End Jelly_Core_Field_Slug
